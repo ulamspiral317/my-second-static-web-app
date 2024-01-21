f418dcf4b02a3b1ef4ae70707e8a9c3d28f2772e
Takuto Masuda <takuto.masuda317@gmail.com>
2024-01-21 18:50:33 +0900

    add gif

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div id="app">
+    <HelloWorld msg="Hello World" />
+    <Gasket msg="シェルピンスキーのギャスケット" />
+  </div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Gasket from "./components/Gasket.vue";
+
 export default {
   name: "App",
-  data() {
-    return {
-      value: "World"
-    };
+  components: {
+    HelloWorld,
+    Gasket
   }
 };
 </script>
---
+++ src/components/Gasket.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="gasket">
+    <img src="@/assets/sierpinski_zooming.gif" alt="シェルピンスキーのギャスケット" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gasket",
+  props: {
+    msg: String
+  }
+};
+</script>
